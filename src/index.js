986941613e420f0cbddec5524d8daf6c2b3a1a5e
Nicholas D'Amico <nickalan82@icloud.com>
2018-02-04 11:43:12 -0500

    init upload, added App functional component and SearchBar Class component that has a controlled input element.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/SearchBar';
+const API_KEY = 'AIzaSyAeaSmAGYwENVogz724rh88kMOQlinm8fg';
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+// Create a new Component should produce HTML
+const App = () => {
+    return(
+      <div>
+        <h1>Youtube API Video Search</h1>
+        <SearchBar />
+      </div>
+    )
+}
+// Append into the Dom... The Component we are building above.
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  <App />,
+  document.querySelector('.container')
+);
